d42c2e40beacd5bd89c75518a6fbcc5af2d4f585
serhioses <serega.mirosh@gmail.com>
2025-04-25 17:34:17 +0300

    Initial commint

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from "eslint-plugin-react-refresh";
+import stylistic from '@stylistic/eslint-plugin';
+
+const config = tseslint.config(
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: [js.configs.recommended] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  pluginReact.configs.flat.recommended,
+  reactHooks.configs["recommended-latest"],
+  reactRefresh.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: { '@stylistic': stylistic },
+    rules: {
+      '@stylistic/semi': 'error',
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/jsx-quotes': ['error', 'prefer-double'],
+      '@stylistic/indent': ['error', 4],
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      'react/react-in-jsx-scope': 'off',
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  }
+);
+
+export default config;
---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+export default {
+    tabWidth: 4,
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 100,
+};
